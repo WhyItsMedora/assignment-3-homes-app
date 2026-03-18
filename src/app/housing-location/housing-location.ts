f21ddc009f2472dcc3caf4e1ac0f5dfb4fc7eedc
WhyItsMedora <medorasailer@gmail.com>
2026-03-17 19:25:45 -0500

    Integrated details page into the application

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { HousingLocationInfo } from '../housing-location-info';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterLink, RouterModule, RouterOutlet],
   templateUrl: './housing-location.html',
   styleUrl: './housing-location.css',
 })
